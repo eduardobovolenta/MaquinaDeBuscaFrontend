160539552af9f8734ab5bb18c9edc6de0476ddad
eduardobovolenta <eduardoalbuquerque10@hotmail.com>
2019-06-11 18:05:50 -0300

    Testes de requisições

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 import { HomePage } from './home.page';
+
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'HomePage',
+  templateUrl: 'home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
 
 @NgModule({
   imports: [
@@ -20,4 +32,23 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+
+@Injectable()
+export class HomePageModule {
+  private API_URL = 'http://localhost:8082/maquina-de-busca/'
+  results: Observable<any>;
+
+  constructor(public http: Http) { }
+  
+  links: any[];
+
+  pegarLinks() {
+
+      let url = this.API_URL + 'link/link';
+
+      this.http.get(url)
+        .subscribe((result: any) => {
+          this.links = result;
+        })
+  }
+}
